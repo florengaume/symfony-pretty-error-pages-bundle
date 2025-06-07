a6f8fe2eedd3cc141fe1f1c065cbca628ea09729
Florent Gaume <florentgaume@MacBookPro.lan>
2025-06-08 01:14:56 +0200

    Ajout de l'override template base

--- src/Command/GenerateErrorPagesCommand.php
+++ src/Command/GenerateErrorPagesCommand.php
@@ -24,18 +24,25 @@ class GenerateErrorPagesCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $lang = $input->getOption('lang');
-        $templatePath = __DIR__ . '/../../templates/error_base.html.twig';
+
+        // Gestion claire de l'override du template
+        $templateOverridesPath = getcwd() . '/templates/bundles/PrettyErrorPagesBundle/error_base.html.twig';
+        $templateDefaultPath = __DIR__ . '/../../templates/error_base.html.twig';
+
+        $templatePath = file_exists($templateOverridesPath) ? $templateOverridesPath : $templateDefaultPath;
+
         $translationPath = __DIR__ . '/../../translations/messages.' . $lang . '.yaml';
         $outputDir = getcwd() . '/templates/bundles/TwigBundle/Exception/';
 
         if (!file_exists($templatePath)) {
-            $output->writeln("<error>Template de base introuvable : $templatePath</error>");
+            $output->writeln("<error>Template introuvable : $templatePath</error>");
             return Command::FAILURE;
         }
         if (!file_exists($translationPath)) {
-            $output->writeln("<error>Fichier de langue introuvable : $translationPath</error>");
+            $output->writeln("<error>Fichier langue introuvable : $translationPath</error>");
             return Command::FAILURE;
         }
+
         @mkdir($outputDir, 0777, true);
 
         $template = file_get_contents($templatePath);
